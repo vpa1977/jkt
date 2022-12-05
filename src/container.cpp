74600bdbbab1e76629a441b391895e57af2e5038
vpa1977 <vladimir.petko@canonical.com>
2022-12-06 08:41:25 +1300

    link debug poco library

---
+++ src/container.cpp
@@ -0,0 +1,7 @@
+#include "Poco/Crypto/PKCS12Container.h"
+
+
+void OpenContainer()
+{
+    Poco::Crypto::PKCS12Container containe("","");
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include "container.h"
+
 int main(int argc, char **argv)
 {
 	printf("jkt - manage java keystore\n");
-
+    OpenContainer();
 }
 
