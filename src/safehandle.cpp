2fa9f6e60c36725680de44ac772492cdc961e2c8
vpa1977 <vladimir.petko@canonical.com>
2023-01-11 15:24:01 +1300

    openssl safehandle specialisations

--- src/safehandle.cpp
+++ src/safehandle.cpp
@@ -1,5 +1,7 @@
 #include "safehandle.h"
 
+#include <openssl/pkcs12.h>
+
 namespace jks
 {
 namespace util
@@ -28,6 +30,20 @@ template <> void SafeHandle<PKCS12 *>::release(PKCS12 *x)
 {
 	if (x != nullptr)
 		PKCS12_free(x);
+}
+
+template <> void SafeHandle<STACK_OF(PKCS7) *>::release(STACK_OF(PKCS7) * x)
+{
+	if (x != nullptr)
+		sk_PKCS7_pop_free(safes, PKCS7_free);
+}
+
+template <>
+void SafeHandle<STACK_OF(PKCS12_SAFEBAG) *>::release(STACK_OF(PKCS12_SAFEBAG) *
+						     x)
+{
+	if (x != nullptr)
+		sk_PKCS12_SAFEBAG_pop_free(bags, PKCS12_SAFEBAG_free);
 }
 
 }
